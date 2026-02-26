2c7e79d33b5b6b023ae81a9e90c969c11a2e33ce
Kizore0102 <ccruzphenrique@gmail.com>
2026-02-26 20:34:59 -0300

    Versao inicial - prazo de 5 dias

---
+++ script.js
@@ -0,0 +1,20 @@
+function print(...args) {
+    console.log(...args)
+}
+function Get_innerHTML(_id, value) {
+    document.getElementById(_id).innerHTML = value
+}
+function Get_id_click(_id, action) {
+    document.getElementById(_id).addEventListener("click", action)
+}
+function Debugger_chen() {
+    print("-----------------------------------------Chen: 30.000k?????------------------------------------")
+}
+
+print("coisa")
+let chen_level = 0
+Get_id_click("btn", () => {
+    print("chen")
+    chen_level += 1
+    Get_innerHTML("label-chen", `Lv.${chen_level}`)
+})
