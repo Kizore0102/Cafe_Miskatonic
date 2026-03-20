d18f363c94505afcb3a150cb065e4922815a8090
Kizore0102 <ccruzphenrique@gmail.com>
2026-03-20 10:54:57 -0300

    Adicionado: Contatos e icones

--- script.js
+++ script.js
@@ -1,22 +1,10 @@
-function print(...args) {
-    console.log(...args)
-}
-function Get_innerHTML(_id, value) {
-    document.getElementById(_id).innerHTML = value
-}
-function Get_id_click(_id, action) {
-    document.getElementById(_id).addEventListener("click", action)
-}
-
-const _button = document.querySelector(".btn");
 const _cardapio_btn = document.querySelectorAll(".btn")
 
+_cardapio_btn.forEach(_button => { _button.addEventListener("click", ButtonPrint) })
 
-
-function ButtonPrint(el_event) {
+function ButtonPrint() {
     console.log("Nyarlatothep desconfia de que você achou os produtos muito antiquados....")
 }
 
-_button.addEventListener("click", ButtonPrint)
 
-_cardapio_btn.addEventListener("click", ButtonPrint)
+
